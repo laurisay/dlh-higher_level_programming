3dd70fb79712687892b8fc799ac89f83c692b9fe
laurisay <assamsayah5@gmail.com>
2026-05-11 09:39:02 +0000

    1-element_at.py

---
+++ python-data_structures/1-element_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    # Variable to store the length of list 🛃 #
+    len_list = len(my_list)
+    # checking if negative or greater than length list 🈂️#
+    if idx < 0 or idx >= len_list:
+        return None
+    # Returning element at idx 🆗#
+    return my_list.pop(idx)
