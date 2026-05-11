72dc3ed37e49b9ae740d9ea1e96760fb0a0ec3e7
laurisay <assamsayah5@gmail.com>
2026-05-11 09:29:16 +0000

    0-print_list_integer.py

---
+++ python-data_structures/0-print_list_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_list_integer(my_list=[]):
+    # Variable to store the length of list 🛃 #
+    len_list = len(my_list)
+    # iterating into list 📶#
+    for i in range(len_list):
+        print("{:d}".format(my_list[i]))
