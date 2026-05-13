c1612678f8f723182a867db74f193f4012e39776
laurisay <assam.sayah5@gmail.com>
2026-05-13 07:23:25 +0000

    5-number_keys.py

---
+++ python-more_data_structures/5-number_keys.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def number_keys(a_dictionary):
+    counter = 0
+    for ky, valu in a_dictionary.items():
+        counter += 1
+    return counter
+"""
+    return len of dictionary also works
+    example: return (len(a_dictionary))
+
+"""
