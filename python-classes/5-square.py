2193649fde7807b5162c13baecd7bbce3225a3bc
laurisay <assamsayah5@gmail.com>
2026-05-24 00:26:02 +0000

    5-square.py

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -1,38 +1,43 @@
 #!/usr/bin/python3
 
-
 """
 This module creates a new class Square
 """
 
 
 class Square:
-    """ __init__ method class """
+    """Defines a square"""
+
     def __init__(self, size=0):
-            self.__size = size
+        self.size = size
 
     def area(self):
-        """ Return the area of square """
-        return (self.__size * self.__size)
+        """Return the area of square"""
+
+        return self.__size * self.__size
 
     @property
     def size(self):
-        """ Return size of square """
+        """Return size of square"""
+
         return self.__size
 
     @size.setter
     def size(self, value):
-        """ Set the size of square """
-        if (type(value) != int):
-            raise ValueError('size must be an integer')
-        elif value < 0:
-            raise ValueError('size must be >= 0')
-        else:
-            self.__size = value
+        """Set the size of square"""
+
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = value
 
     def my_print(self):
-        """ Print a square """
-        if self.__size is 0:
+        """Print a square"""
+
+        if self.__size == 0:
             print()
         else:
             for col in range(self.__size):
