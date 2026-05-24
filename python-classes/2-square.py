9dd6c751900eb95780858af7eb74d95ecee9f22e
laurisay <assamsayah5@gmail.com>
2026-05-24 00:05:26 +0000

    ccorrection 2-square.py

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python3
+
+
+"""
+This module creates a new class Square
+"""
+
+
 class Square:
-    """Class Square with size validation"""
+    """ init method class """
     def __init__(self, size=0):
-        self.__size = size
-        if type(size) != int:
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
+        if (type(size) != int):
+            raise ValueError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = size
