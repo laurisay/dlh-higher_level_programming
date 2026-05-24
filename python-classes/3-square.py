49ed176a666de241154b708409f7069201811e12
laurisay <assamsayah5@gmail.com>
2026-05-24 00:18:37 +0000

    ccorrection 3-square.py

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,21 +1,24 @@
 #!/usr/bin/python3
 
-
 """
 This module creates a new class Square
 """
 
 
 class Square:
-    """ init method class """
+    """Defines a square"""
+
     def __init__(self, size=0):
-        if (type(size) != int):
-            raise TypeError('size must be an integer')
-        elif size < 0:
-            raise ValueError('size must be >= 0')
-        else:
-            self.__size = size
+
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
 
     def area(self):
-        """ Return the area of square """
-        return (self.__size * self.__size)
+        """Return the area of the square"""
+
+        return self.__size * self.__size
