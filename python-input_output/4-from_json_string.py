6cf4aba04f42e765e074b8c4d6e2c8808afc722e
laurisay <assamsayah5@gmail.com>
2026-06-01 07:59:56 +0000

    4-from_json_string.py

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Module that creates a from_json_string function
+"""
+import json
+
+
+def from_json_string(my_str):
+    """Returns an object (Python data structure) represented by a JSON string
+
+    Args:
+        my_str (str): The JSON string to convert to Python object
+    Returns:
+        object: Python data structure represented by the JSON string
+    """
+    return json.loads(my_str)
